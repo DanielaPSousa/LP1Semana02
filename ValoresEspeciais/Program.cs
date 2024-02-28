d40c00e01253cc63ca75e3f2077ffd4929deea95
Daniela Sousa <danielasousa.info@gmail.com>
2024-02-28 22:41:06 +0000

    add maximum and minimum values

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,16 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"byte:{byte.MinValue} ; {byte.MaxValue}");
+            Console.WriteLine($"int:{int.MinValue} ; {int.MaxValue}");
+            Console.WriteLine($"short:{short.MinValue} ; {long.MaxValue}");
+            Console.WriteLine($"long:{long.MinValue} ; {long.MaxValue}");
+            Console.WriteLine($"ulong:{ulong.MinValue} ; {ulong.MaxValue}");
+            Console.WriteLine($"ushort:{ushort.MinValue} ; {ushort.MaxValue}");
+            Console.WriteLine($"uint:{uint.MinValue} ; {uint.MaxValue}");
+            Console.WriteLine($"float:{float.MinValue} ; {float.MaxValue}");
+            Console.WriteLine($"byte:{decimal.MinValue} ; {decimal.MaxValue}");
+            Console.WriteLine($"double:{byte.MinValue} ; {double.MaxValue}");
         }
     }
 }

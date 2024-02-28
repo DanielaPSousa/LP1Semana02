c5650c15c2e49a554c360ed2025a028e83ac98cb
Daniela Sousa <danielasousa.info@gmail.com>
2024-02-28 22:07:06 +0000

    Ex.5 done

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,17 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insere a altura do cilindro");
+            float al= float.Parse(altura);
+
+            Console.Write("Insere o raio do cilindro");
+            float ra= float.Parse(raio);
+
+            float volume= MathF.PI * MathF.Pow (ra, 2) * al;
+            float superficie= 2*MathF.PI * ra (ra + al);
+
+            Console.WriteLine($"O volume do cilindro é {volume}");
+            Console.WriteLine($"A área da superficie é{superficie}");
         }
     }
 }

4f986bfc1990232ffb7abac13b8307287ac3389a
Daniela Sousa <danielasousa.info@gmail.com>
2024-02-29 23:33:44 +0000

    add questions, ask the user for a question and the chatbot does not recognize the question, it informs the user

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -2,11 +2,40 @@
 
 namespace ChatGPTOne
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Welcome! Ask me a question ");
+            question = Console.ReadLine();
+
+            // Verificar se trata de uma questão que reconheça e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                case "Who painted Monalisa?":
+                    response = "Leonardo Da Vinci";
+                    break;
+                case "What's your favorite food?":
+                    response = "I don't have a favorite food.. I'm a machine remember?";
+                    break;
+                case "Are you dumb?":
+                    response = "What kind of question is that?! Of course not! outrageous..";
+                    break;
+                case "40+50?":
+                    response = "90";
+                    break;
+                default:
+                    response = "My systems failed..ask me another question";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
         }
     }
 }
